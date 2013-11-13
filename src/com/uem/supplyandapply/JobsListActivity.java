d573d35aa2d8ab78f09bd74bfd7aaa8effb73a14
Andrea Wu <andreaowu@berkeley.edu>
2013-11-12 22:09:57 -0800

    initial commit

---
+++ src/com/uem/supplyandapply/JobsListActivity.java
@@ -0,0 +1,22 @@
+package com.uem.supplyandapply;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class JobsListActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.jobs_list_layout);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.jobs_list, menu);
+		return true;
+	}
+
+}
