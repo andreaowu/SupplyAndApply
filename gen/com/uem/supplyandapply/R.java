c8bc667f49d14e9ab8f227f6bba8466dc533cf3b
nikki7647 <davidminjia@gmail.com>
2013-11-17 15:36:44 -0800

    layout

--- gen/com/uem/supplyandapply/R.java
+++ gen/com/uem/supplyandapply/R.java
@@ -24,35 +24,46 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000e;
+        public static final int action_settings=0x7f080016;
         public static final int appliance_count=0x7f080006;
         public static final int appliance_image=0x7f080003;
         public static final int application_list=0x7f080002;
         public static final int buttons_container=0x7f080001;
-        public static final int current=0x7f080008;
-        public static final int currentJobsList=0x7f08000a;
-        public static final int currentList=0x7f080009;
+        public static final int curent_job_gridView=0x7f08000a;
+        public static final int current=0x7f080010;
+        public static final int currentJobsList=0x7f080012;
+        public static final int currentList=0x7f080011;
+        public static final int current_job_addApplianceButton=0x7f08000e;
+        public static final int current_job_address=0x7f080008;
+        public static final int current_job_customer=0x7f080007;
+        public static final int current_job_progress=0x7f080009;
+        public static final int current_job_progressBar=0x7f08000c;
+        public static final int current_job_removeApplianceButton=0x7f08000b;
+        public static final int current_job_viewpartButton=0x7f08000d;
         public static final int decrement_button=0x7f080005;
         public static final int increment_button=0x7f080004;
         public static final int input_address=0x7f080000;
-        public static final int past=0x7f08000b;
-        public static final int pastJobsList=0x7f08000d;
-        public static final int pastList=0x7f08000c;
-        public static final int textView1=0x7f080007;
+        public static final int past=0x7f080013;
+        public static final int pastJobsList=0x7f080015;
+        public static final int pastList=0x7f080014;
+        public static final int textView1=0x7f08000f;
     }
     public static final class layout {
         public static final int add_job_appliances_layout=0x7f030000;
         public static final int appliance_list_row=0x7f030001;
-        public static final int jobs_list_layout=0x7f030002;
+        public static final int current_job_layout=0x7f030002;
+        public static final int jobs_list_layout=0x7f030003;
     }
     public static final class menu {
-        public static final int jobs_list=0x7f070000;
+        public static final int current_job=0x7f070000;
+        public static final int jobs_list=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int all_jobs=0x7f050003;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_current_job=0x7f050004;
     }
     public static final class style {
         /** 
