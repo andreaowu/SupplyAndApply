e8cb2e98b93f363ecd7fa925e8a1e59e3835288c
Alex Texter <itstexter@gmail.com>
2013-12-09 15:03:54 -0800

    sharedpreferences

--- src/com/uem/supplyandapply/Constants.java
+++ src/com/uem/supplyandapply/Constants.java
@@ -16,4 +16,5 @@ public class Constants {
     public static final String ADDRESS="address";
     public static final String NAME="name";
 	public static final String APPLIANCE = "appliance";
+    public static final String SEENJOBSPAGE = "seenjobspage";
 }
--- src/com/uem/supplyandapply/JobsListActivity.java
+++ src/com/uem/supplyandapply/JobsListActivity.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
@@ -104,7 +108,22 @@ public class JobsListActivity extends Activity {
 				}
 			}
 		}
-	}
+
+        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean seenJobsPage = sharedPreferences.getBoolean(Constants.SEENJOBSPAGE, false);
+        if (!seenJobsPage) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Welcome to Supply and Apply! This is the main screen where you will see all your current and past jobs.")
+                    .setCancelable(false)
+                    .setPositiveButton("Dismiss", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            sharedPreferences.edit().putBoolean(Constants.SEENJOBSPAGE, true);
+                        }
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
